931bf0945f5c02bf351924941ac0b97e47bed85c
samdarbonne <samdarbonne@gmail.com>
2016-08-19 11:18:52 -0700

    rave button now changes background on click

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,10 @@
 // sanity check
 console.log("app.js linked.");
 
-// $('.rave').on('click', setInterval(function(){
-// 	var red = Math.floor(Math.random() * 255);
-// 	var green = Math.floor(Math.random() * 255);
-// 	var blue = Math.floor(Math.random() * 255);
-// 	$('body').css('background-color', 'rgb(' + red +', '+green+', '+blue+')');
-// }), 50000);
+$('.rave').on('click', function(){
+	var red = Math.floor(Math.random() * 255);
+	var green = Math.floor(Math.random() * 255);
+	var blue = Math.floor(Math.random() * 255);
+	$('body').css('background-color', 'rgb(' + red +', '+green+', '+blue+')');
+	$('header').css('background-color', 'rgb(' + red +', '+green+', '+blue+')');
+});
