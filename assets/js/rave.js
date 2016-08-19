12540074f9a7bfa8e34229700e6989fbd6125bcd
samdarbonne <samdarbonne@gmail.com>
2016-08-19 16:50:33 -0700

    adds tic tac toe

--- assets/js/rave.js
+++ assets/js/rave.js
@@ -1,6 +1,7 @@
-$('.rave').on('click', setInterval(function(){
+$('.rave').on('click', setInterval(function(event){
 	var red = Math.floor(Math.random() * 255);
 	var green = Math.floor(Math.random() * 255);
 	var blue = Math.floor(Math.random() * 255);
+	debugger
 	$('body').css('background-color', 'rgb(' + red +', '+green+', '+blue+')');
 }), 50000);
