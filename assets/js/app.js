b958f1b5eceb49aac8656c1922ad7daef1303ec2
samdarbonne <samdarbonne@gmail.com>
2016-08-20 07:54:34 -0700

    game logic works, stalemate not detected

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,9 +2,21 @@
 console.log("app.js linked.");
 
 $('.rave').on('click', function(){
+	randomLightColorBG();
+});
+
+function randomColorBG() {
 	var red = Math.floor(Math.random() * 255);
 	var green = Math.floor(Math.random() * 255);
 	var blue = Math.floor(Math.random() * 255);
 	$('body').css('background-color', 'rgb(' + red +', '+green+', '+blue+')');
 	$('header').css('background-color', 'rgb(' + red +', '+green+', '+blue+')');
-});
+}
+
+function randomLightColorBG() {
+	var red = Math.floor((Math.random() * 155) + 100);
+	var green = Math.floor((Math.random() * 155) + 100);
+	var blue = Math.floor((Math.random() * 155) + 100);
+	$('body').css('background-color', 'rgb(' + red +', '+green+', '+blue+')');
+	$('header').css('background-color', 'rgb(' + red +', '+green+', '+blue+')');
+}
